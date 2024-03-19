09fa56bad1f3bd1ec011dc9910814affb3efa77c
Dilan <dilan@dilanxd.com>
2024-03-18 19:04:18 -0500

    update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ for (const line of lines) {
   const from = redirectLine[0].split('/').filter((x) => x.length > 0);
   const to = redirectLine[1];
   const redirectDir = path.join(dst, ...from);
-  fs.mkdirSync(path.dirname(redirectDir), { recursive: true });
+  fs.mkdirSync(redirectDir, { recursive: true });
   fs.writeFileSync(
     path.join(redirectDir, 'index.html'),
     `<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><title>Redirecting...</title><script>window.location.replace(\`${to}\`);</script></head></html>`
